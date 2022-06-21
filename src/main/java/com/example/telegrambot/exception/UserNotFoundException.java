ad445228cf3099e7b0475f1fa08942b135f39377
mirkomilablayev <mirkomilablayev777@gmail.com>
2022-06-21 11:18:48 +0500

    Upgrated

--- src/main/java/com/example/telegrambot/exception/UserNotFoundException.java
+++ src/main/java/com/example/telegrambot/exception/UserNotFoundException.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-//@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
     private String message;
 }
